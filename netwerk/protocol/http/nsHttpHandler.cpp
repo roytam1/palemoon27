b67eab9a2a85ff5b9cd76ad67c17a585ebee28dc
wolfbeast <mcwerewolf@gmail.com>
2016-04-18 00:22:16 +0200

    Fix UA handling build bustage

--- netwerk/protocol/http/nsHttpHandler.cpp
+++ netwerk/protocol/http/nsHttpHandler.cpp
@@ -329,7 +329,7 @@ nsHttpHandler::Init()
     if (mCompatFirefoxEnabled) {
       mProductSub.AssignLiteral(MOZILLA_UAVERSION);
     } else {
-      mProductSub.AssignLiteral(MOZ_UA_BUILDID);
+      mProductSub.AssignLiteral("20100101");
     }
 #endif
     if (mProductSub.IsEmpty() && appInfo)
@@ -892,7 +892,7 @@ nsHttpHandler::PrefsChanged(nsIPrefBranch *prefs, const char *pref)
           mProductSub.AssignLiteral(MOZILLA_UAVERSION);          
         } else {
           mMisc.AssignLiteral("rv:" MOZILLA_UAVERSION);
-          mProductSub.AssignLiteral(MOZ_UA_BUILDID);
+          mProductSub.AssignLiteral("20100101");
         }
         nsCOMPtr<nsIXULAppInfo> appInfo = 
             do_GetService("@mozilla.org/xre/app-info;1");

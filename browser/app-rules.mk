ab1513a9846d132381035886f00eb4a6fc6df0e3
Matt A. Tobin <email@mattatobin.com>
2016-05-09 19:30:56 -0400

    Obliterate Browser 38

--- browser/app-rules.mk
+++
@@ -1,4 +0,0 @@
-PURGECACHES_DIRS = $(DIST)/bin/browser
-ifdef MOZ_METRO
-PURGECACHES_DIRS += $(DIST)/bin/metro
-endif
--- browser/components/safebrowsing/content/test/head.js
+++
@@ -1,5 +0,0 @@
-// Force SafeBrowsing to be initialized for the tests
-Services.prefs.setCharPref("urlclassifier.malwareTable", "test-malware-simple");
-Services.prefs.setCharPref("urlclassifier.phishTable", "test-phish-simple");
-SafeBrowsing.init();
-

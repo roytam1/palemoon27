3f7750c4855bcbbcebebcb5e4c54003551e9b1cf
wolfbeast <mcwerewolf@gmail.com>
2016-06-05 16:15:16 +0200

    Switch login manager to use DOMFormHasPassword instead of DOMContentLoaded.
    LoginManagerContent.onContentLoaded no longer exists.

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -1174,12 +1174,28 @@ var gBrowserInit = {
     // If the user manually opens the download manager before the timeout, the
     // downloads will start right away, and getting the service again won't hurt.
     setTimeout(function() {
+      try {
       Services.downloads;
       let DownloadTaskbarProgress =
         Cu.import("resource://gre/modules/DownloadTaskbarProgress.jsm", {}).DownloadTaskbarProgress;
       DownloadTaskbarProgress.onBrowserWindowLoad(window);
+      } catch(ex) {
+      Cu.reportError(ex);
+      }
     }, 10000);
 
+    // Load the Login Manager data from disk off the main thread, some time
+    // after startup.  If the data is required before the timeout, for example
+    // because a restored page contains a password field, it will be loaded on
+    // the main thread, and this initialization request will be ignored.
+    setTimeout(function() {
+      try {
+        Services.logins;
+      } catch (ex) {
+        Cu.reportError(ex);
+      }
+    }, 3000);
+    
     // The object handling the downloads indicator is also initialized here in the
     // delayed startup function, but the actual indicator element is not loaded
     // unless there are downloads to be displayed.

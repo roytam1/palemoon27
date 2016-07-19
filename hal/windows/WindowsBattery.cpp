4eca9ddcedbf84319ace8b99565fd9f9a2136858
wolfbeast <mcwerewolf@gmail.com>
2016-07-19 02:43:39 +0200

    Remove dead code from battery HAL

--- hal/windows/WindowsBattery.cpp
+++ hal/windows/WindowsBattery.cpp
@@ -63,94 +63,71 @@ BatteryWindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 void
 EnableBatteryNotifications()
 {
-  if (IsVistaOrLater()) {
-    // RegisterPowerSettingNotification is from Vista or later.
-    // Use this API if available.
-    HMODULE hUser32 = GetModuleHandleW(L"USER32.DLL");
-    if (!sRegisterPowerSettingNotification)
-      sRegisterPowerSettingNotification = (decltype(RegisterPowerSettingNotification)*)
-        GetProcAddress(hUser32, "RegisterPowerSettingNotification");
-    if (!sUnregisterPowerSettingNotification)
-      sUnregisterPowerSettingNotification = (decltype(UnregisterPowerSettingNotification)*)
-        GetProcAddress(hUser32, "UnregisterPowerSettingNotification");
+  HMODULE hUser32 = GetModuleHandleW(L"USER32.DLL");
+  if (!sRegisterPowerSettingNotification)
+    sRegisterPowerSettingNotification = (decltype(RegisterPowerSettingNotification)*)
+      GetProcAddress(hUser32, "RegisterPowerSettingNotification");
+  if (!sUnregisterPowerSettingNotification)
+    sUnregisterPowerSettingNotification = (decltype(UnregisterPowerSettingNotification)*)
+      GetProcAddress(hUser32, "UnregisterPowerSettingNotification");
 
-    if (!sRegisterPowerSettingNotification ||
-        !sUnregisterPowerSettingNotification) {
-      NS_ASSERTION(false, "Canot find PowerSettingNotification functions.");
-      return;
-    }
-
-    // Create custom window to watch battery event
-    // If we can get Goanna's window handle, this is unnecessary.
-
-    if (sHWnd == nullptr) {
-      WNDCLASSW wc;
-      HMODULE hSelf = GetModuleHandle(nullptr);
-
-      if (!GetClassInfoW(hSelf, L"MozillaBatteryClass", &wc)) {
-        ZeroMemory(&wc, sizeof(WNDCLASSW));
-        wc.hInstance = hSelf;
-        wc.lpfnWndProc = BatteryWindowProc;
-        wc.lpszClassName = L"MozillaBatteryClass";
-        RegisterClassW(&wc);
-      }
-
-      sHWnd = CreateWindowW(L"MozillaBatteryClass", L"Battery Watcher",
-                            0, 0, 0, 0, 0,
-                            nullptr, nullptr, hSelf, nullptr);
-    }
-
-    if (sHWnd == nullptr) {
-      return;
-    }
-
-    sPowerHandle =
-      sRegisterPowerSettingNotification(sHWnd,
-                                        &GUID_ACDC_POWER_SOURCE,
-                                        DEVICE_NOTIFY_WINDOW_HANDLE);
-    sCapacityHandle =
-      sRegisterPowerSettingNotification(sHWnd,
-                                        &GUID_BATTERY_PERCENTAGE_REMAINING,
-                                        DEVICE_NOTIFY_WINDOW_HANDLE);
-  } else
-  {
-    // for Windows XP.  If we remove Windows XP support,
-    // we should remove timer-based power notification
-    sUpdateTimer = do_CreateInstance(NS_TIMER_CONTRACTID);
-    if (sUpdateTimer) {
-      sUpdateTimer->InitWithFuncCallback(UpdateHandler,
-                                         nullptr,
-                                         Preferences::GetInt("dom.battery.timer",
-                                                             30000 /* 30s */),
-                                         nsITimer::TYPE_REPEATING_SLACK);
-    } 
+  if (!sRegisterPowerSettingNotification ||
+      !sUnregisterPowerSettingNotification) {
+    NS_ASSERTION(false, "Canot find PowerSettingNotification functions.");
+    return;
   }
+
+  // Create custom window to watch battery event
+  // If we can get Goanna's window handle, this is unnecessary.
+
+  if (sHWnd == nullptr) {
+    WNDCLASSW wc;
+    HMODULE hSelf = GetModuleHandle(nullptr);
+
+    if (!GetClassInfoW(hSelf, L"MozillaBatteryClass", &wc)) {
+      ZeroMemory(&wc, sizeof(WNDCLASSW));
+      wc.hInstance = hSelf;
+      wc.lpfnWndProc = BatteryWindowProc;
+      wc.lpszClassName = L"MozillaBatteryClass";
+      RegisterClassW(&wc);
+    }
+
+    sHWnd = CreateWindowW(L"MozillaBatteryClass", L"Battery Watcher",
+                          0, 0, 0, 0, 0,
+                          nullptr, nullptr, hSelf, nullptr);
+  }
+
+  if (sHWnd == nullptr) {
+    // Couldn't get window; bail.
+    return;
+  }
+
+  sPowerHandle =
+    sRegisterPowerSettingNotification(sHWnd,
+                                      &GUID_ACDC_POWER_SOURCE,
+                                      DEVICE_NOTIFY_WINDOW_HANDLE);
+  sCapacityHandle =
+    sRegisterPowerSettingNotification(sHWnd,
+                                      &GUID_BATTERY_PERCENTAGE_REMAINING,
+                                      DEVICE_NOTIFY_WINDOW_HANDLE);
 }
 
 void
 DisableBatteryNotifications()
 {
-  if (IsVistaOrLater()) {
-    if (sPowerHandle) {
-      sUnregisterPowerSettingNotification(sPowerHandle);
-      sPowerHandle = nullptr;
-    }
+  if (sPowerHandle) {
+    sUnregisterPowerSettingNotification(sPowerHandle);
+    sPowerHandle = nullptr;
+  }
 
-    if (sCapacityHandle) {
-      sUnregisterPowerSettingNotification(sCapacityHandle);
-      sCapacityHandle = nullptr;
-    }
+  if (sCapacityHandle) {
+    sUnregisterPowerSettingNotification(sCapacityHandle);
+    sCapacityHandle = nullptr;
+  }
 
-    if (sHWnd) {
-      DestroyWindow(sHWnd);
-      sHWnd = nullptr;
-    }
-  } else
-  {
-    if (sUpdateTimer) {
-      sUpdateTimer->Cancel();
-      sUpdateTimer = nullptr;
-    }
+  if (sHWnd) {
+    DestroyWindow(sHWnd);
+    sHWnd = nullptr;
   }
 }
 

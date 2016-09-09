dc358491690c790be666c0c851c52405c5cd6c61
Matt A. Tobin <email@mattatobin.com>
2016-09-09 04:56:11 -0400

    Import the Statusbar - Part 4: Preferences (browser/app/profile/firefox.js)

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1390,3 +1390,51 @@ pref("general.useragent.override.addons.mozilla.org","Mozilla/5.0 (Linux; X11; r
 #else
 pref("general.useragent.override.addons.mozilla.org","Mozilla/5.0 (Windows NT 6.1; WOW64; rv:24.9) Gecko/20100101 Firefox/24.9 (Pale Moon)");
 #endif
+
+// ****************** s4e prefs ******************
+pref("status4evar.addonbar.borderStyle", false);
+pref("status4evar.addonbar.closeButton", false);
+pref("status4evar.addonbar.legacyShim", true);
+pref("status4evar.addonbar.windowGripper", true);
+pref("status4evar.advanced.showWarning", true);
+pref("status4evar.advanced.status.detectFullScreen", true);
+pref("status4evar.advanced.status.detectVideo", true);
+pref("status4evar.advanced.urlbar.forceBinding", false);
+pref("status4evar.download.button.action", 1);
+pref("status4evar.download.button.action.command", "");
+pref("status4evar.download.color.active", "#33FF33");
+pref("status4evar.download.color.paused", "#808080");
+pref("status4evar.download.force", true);
+pref("status4evar.download.label", 0);
+pref("status4evar.download.label.force", false);
+pref("status4evar.download.notify.animate", true);
+pref("status4evar.download.notify.timeout", 60);
+pref("status4evar.download.progress", 1);
+pref("status4evar.download.tooltip", 1);
+pref("status4evar.firstRun", true);
+pref("status4evar.firstRun.australis", true);
+//pref("status4evar.progress.tab", 1);
+//pref("status4evar.progress.tab.css", "#33FF33");
+//pref("status4evar.progress.tab.style", true);
+pref("status4evar.progress.toolbar.css", "#33FF33");
+pref("status4evar.progress.toolbar.force", false);
+pref("status4evar.progress.toolbar.style", false);
+pref("status4evar.progress.toolbar.style.advanced", false);
+pref("status4evar.progress.urlbar", 1);
+pref("status4evar.progress.urlbar.css", "#33FF33");
+pref("status4evar.progress.urlbar.style", true);
+pref("status4evar.progress.urlbar.style.advanced", false);
+pref("status4evar.status", 1);
+pref("status4evar.status.default", true);
+pref("status4evar.status.network", true);
+pref("status4evar.status.network.xhr", true);
+pref("status4evar.status.timeout", 10);
+pref("status4evar.status.linkOver", 1);
+pref("status4evar.status.linkOver.delay.show", 70);
+pref("status4evar.status.linkOver.delay.hide", 150);
+pref("status4evar.status.toolbar.maxLength", 0);
+pref("status4evar.status.urlbar.align", 0);
+pref("status4evar.status.urlbar.color", "GrayText");
+pref("status4evar.status.urlbar.position", 33);
+pref("status4evar.status.popup.invertMirror", false);
+pref("status4evar.status.popup.mouseMirror", true);

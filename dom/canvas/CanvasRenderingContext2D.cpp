2962f40b44cd52163ee7ae1dba4aa80dd591944e
Pale Moon <git-repo@palemoon.org>
2016-09-14 17:22:43 +0200

    Parallelize canvas poisoning.

--- dom/canvas/CanvasRenderingContext2D.cpp
+++ dom/canvas/CanvasRenderingContext2D.cpp
@@ -1631,7 +1631,10 @@ CanvasRenderingContext2D::GetInputStream(const char *aMimeType,
     // Image buffer is always a packed BGRA array (BGRX -> BGR[FF])
     // so always 4-value pixels.
     // GetImageBuffer => SurfaceToPackedBGRA [=> ConvertBGRXToBGRA]
-    for (int32_t j = 0; j < mWidth * mHeight * 4; ++j) {
+    int32_t dataSize = mWidth * mHeight * 4;
+#pragma loop(ivdep)
+#pragma loop(hint_parallel(0))
+    for (int32_t j = 0; j < dataSize; ++j) {
       if (imageBuffer[j] !=0 && imageBuffer[j] != 255)
         imageBuffer[j] += rand() % 3 - 1;
     }

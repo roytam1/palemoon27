d8cdbde6ce5fb1d71b13de6d672309a093bdea7c
Gpower2 <arslanoglou.georgios@gmail.com>
2016-09-17 18:00:13 +0300

    Change WIN_VER and _WIN32_WINNT to 0x0600

--- other-licenses/7zstub/src/7zip/FileManager/Resource/ProgressDialog/StdAfx.h
+++ other-licenses/7zstub/src/7zip/FileManager/Resource/ProgressDialog/StdAfx.h
@@ -3,10 +3,10 @@
 #ifndef __STDAFX_H
 #define __STDAFX_H
 
-#define _WIN32_WINNT 0x0400
+#define _WIN32_WINNT 0x0600
 
 // it's for Windows NT supporting (MENUITEMINFOW)
-#define WINVER 0x0400
+#define WINVER 0x0600
 
 #include <windows.h>
 #include <commctrl.h>

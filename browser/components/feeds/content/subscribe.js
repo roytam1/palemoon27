9998433143c2cf299f6320c45eafb37d36849a38
Pale Moon <git-repo@palemoon.org>
2016-10-05 13:11:54 +0200

    Convert feedwriter to WebIDL and fix access to anonymous content. Also fix a few small nits/sanity checks while at it.
    This resolves #554.

--- browser/components/feeds/content/subscribe.js
+++ browser/components/feeds/content/subscribe.js
@@ -1,4 +1,4 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
@@ -11,7 +11,6 @@ var SubscribeHandler = {
   
   init: function SH_init() {
     this._feedWriter = new BrowserFeedWriter();
-    this._feedWriter.init(window);
   },
 
   writeContent: function SH_writeContent() {
@@ -20,9 +19,5 @@ var SubscribeHandler = {
 
   uninit: function SH_uninit() {
     this._feedWriter.close();
-  },
-  
-  subscribe: function SH_subscribe() {
-    this._feedWriter.subscribe();
   }
 };

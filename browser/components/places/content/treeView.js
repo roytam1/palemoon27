c161d1f40cdb5fb7aba61cd314d7fb30af1655df
janekptacijarabaci <janekptacijarabaci@seznam.cz>
2016-10-14 17:38:47 +0200

    Minor fix: Places - aOldNode is undefined (the best solution)

--- browser/components/places/content/treeView.js
+++ browser/components/places/content/treeView.js
@@ -399,7 +399,7 @@ PlacesTreeView.prototype = {
    */
   _getNewRowForRemovedNode:
   function PTV__getNewRowForRemovedNode(aUpdatedContainer, aOldNode) {
-    if (aOldNode === undefined) {
+    if (aOldNode == undefined) {
       return -1;
     }
     let parent = aOldNode.parent;

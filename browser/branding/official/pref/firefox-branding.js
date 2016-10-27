bc1605ab129d3e4beba2b9f6bfb848bc6fbde674
Pale Moon <git-repo@palemoon.org>
2016-10-28 00:33:44 +0200

    Split some release-specific prefs back out to their individual files.

--- browser/branding/official/pref/firefox-branding.js
+++ browser/branding/official/pref/firefox-branding.js
@@ -3,3 +3,5 @@
 #include ../../shared/pref/preferences.inc
 #include ../../shared/pref/uaoverrides.inc
 
+pref("startup.homepage_override_url","http://www.palemoon.org/releasenotes.shtml");
+pref("app.releaseNotesURL", "http://www.palemoon.org/releasenotes.shtml");
--- browser/branding/unofficial/pref/firefox-branding.js
+++ browser/branding/unofficial/pref/firefox-branding.js
@@ -2,3 +2,6 @@
 #filter emptyLines
 #include ../../shared/pref/preferences.inc
 #include ../../shared/pref/uaoverrides.inc
+
+pref("startup.homepage_override_url","http://www.palemoon.org/unofficial.shtml");
+pref("app.releaseNotesURL", "http://www.palemoon.org/releasenotes.shtml");

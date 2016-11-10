ca4d8ff80ec260c75f7e3803b2bf81122db63f63
Pale Moon <git-repo@palemoon.org>
2016-11-10 16:38:01 +0100

    Lower arbitrary AMO override to 27.9 to prevent discrepancy with our XPI installer.

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1063,12 +1063,12 @@ pref("browser.display.standalone_images.background_color", "#2E3B41");
 // AMO needs "Firefox", obviously - pass on the OS (determined at build time)
 #ifdef XP_UNIX
 #ifdef XP_MACOSX
-pref("general.useragent.override.addons.mozilla.org","Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:28.9) Gecko/20100101 Firefox/28.9 (Pale Moon)");
+pref("general.useragent.override.addons.mozilla.org","Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:27.9) Gecko/20100101 Firefox/27.9 (Pale Moon)");
 #else
-pref("general.useragent.override.addons.mozilla.org","Mozilla/5.0 (Linux; X11; rv:28.9) Gecko/20100101 Firefox/28.9 (Pale Moon)");
+pref("general.useragent.override.addons.mozilla.org","Mozilla/5.0 (Linux; X11; rv:27.9) Gecko/20100101 Firefox/27.9 (Pale Moon)");
 #endif
 #else
-pref("general.useragent.override.addons.mozilla.org","Mozilla/5.0 (Windows NT 6.1; WOW64; rv:28.9) Gecko/20100101 Firefox/28.9 (Pale Moon)");
+pref("general.useragent.override.addons.mozilla.org","Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.9) Gecko/20100101 Firefox/27.9 (Pale Moon)");
 #endif
 
 // ****************** s4e prefs ******************

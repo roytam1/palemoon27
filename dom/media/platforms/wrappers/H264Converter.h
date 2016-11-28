cd3a76330b005931836683ee276897f26e3a8fa6
trav90 <travawine@protonmail.ch>
2016-11-27 18:29:20 -0600

    Check whether H264Converter created its decoder successfully

--- dom/media/platforms/wrappers/H264Converter.h
+++ dom/media/platforms/wrappers/H264Converter.h
@@ -39,6 +39,7 @@ public:
 
   // Return true if mimetype is H.264.
   static bool IsH264(const TrackInfo& aConfig);
+  nsresult GetLastError() const { return mLastError; }
 
 private:
   // Will create the required MediaDataDecoder if need AVCC and we have a SPS NAL.

ea47e5bd62aa04e2985e20c612cb1c6be1946a8e
trav90 <travawine@protonmail.ch>
2016-12-07 18:45:54 -0600

    Disable ffvpx with libav0.8 and earlier

--- dom/media/platforms/ffmpeg/FFmpegH264Decoder.cpp
+++ dom/media/platforms/ffmpeg/FFmpegH264Decoder.cpp
@@ -99,6 +99,7 @@ FFmpegH264Decoder<LIBAV_VER>::DoDecodeFrame(MediaRawData* aSample)
   uint8_t* inputData = const_cast<uint8_t*>(aSample->Data());
   size_t inputSize = aSample->Size();
 
+#if LIBAVCODEC_VERSION_MAJOR >= 54
   if (inputSize && (mCodecID == AV_CODEC_ID_VP8
 #if LIBAVCODEC_VERSION_MAJOR >= 55
       || mCodecID == AV_CODEC_ID_VP9
@@ -140,6 +141,7 @@ FFmpegH264Decoder<LIBAV_VER>::DoDecodeFrame(MediaRawData* aSample)
     }
     return gotFrame ? DecodeResult::DECODE_FRAME : DecodeResult::DECODE_NO_FRAME;
   }
+#endif
   return DoDecodeFrame(aSample, inputData, inputSize);
 }
 
@@ -434,9 +436,11 @@ FFmpegH264Decoder<LIBAV_VER>::GetCodecId(const nsACString& aMimeType)
     return AV_CODEC_ID_VP6F;
   }
 
+#if LIBAVCODEC_VERSION_MAJOR >= 54
   if (aMimeType.EqualsLiteral("video/webm; codecs=vp8")) {
     return AV_CODEC_ID_VP8;
   }
+#endif
 
 #if LIBAVCODEC_VERSION_MAJOR >= 55
   if (aMimeType.EqualsLiteral("video/webm; codecs=vp9")) {

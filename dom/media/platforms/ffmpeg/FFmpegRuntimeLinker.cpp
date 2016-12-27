4d5cec677740d6874f9165d77a2de5f6aa317ab8
trav90 <travawine@protonmail.ch>
2016-12-27 01:56:53 -0600

    Accept libavcodec 57.100+

--- dom/media/platforms/ffmpeg/FFmpegRuntimeLinker.cpp
+++ dom/media/platforms/ffmpeg/FFmpegRuntimeLinker.cpp
@@ -111,8 +111,8 @@ FFmpegRuntimeLinker::Bind(const char* aLibName)
       version = AV_FUNC_55;
       break;
     case 57:
-      if (micro != 100) {
-        // A micro version of 100 indicates that it's FFmpeg (as opposed to LibAV).
+      if (micro < 100) {
+        // A micro version >= 100 indicates that it's FFmpeg (as opposed to LibAV).
         // Due to current AVCodecContext binary incompatibility we can only
         // support FFmpeg at this point.
         return false;

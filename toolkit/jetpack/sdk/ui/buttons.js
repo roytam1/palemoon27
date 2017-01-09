9e96343c42c118ed15746b4933837081c6f801fa
JustOff <Off.Just.Off@gmail.com>
2017-01-09 23:45:53 +0200

    More accurate way to handle location of 'ui/buttons' in toolbars

--- toolkit/jetpack/sdk/ui/buttons.js
+++ toolkit/jetpack/sdk/ui/buttons.js
@@ -13,20 +13,28 @@ module.metadata = {
 
 const { Ci, Cc } = require('chrome');
 const prefs = require('../preferences/service');
-const blist = new Map();
 
-const INSERTION_PREF_ROOT = "extensions.sdk-button-inserted.";
+const buttonsList = new Map();
+const LOCATION_PREF_ROOT = "extensions.sdk-button-location.";
 
 let gWindowListener;
 
-function saveInserted(id) {
-  prefs.set(INSERTION_PREF_ROOT + id, true);
+function getLocation(id) {
+  let toolbarId = "nav-bar", nextItemId = "";
+  let location = prefs.get(LOCATION_PREF_ROOT + id);
+  if (location && location.indexOf(",") !== -1) {
+    [toolbarId, nextItemId] = location.split(",");
+  }
+  return [toolbarId, nextItemId];
 }
 
-function haveInserted(id) {
-  return prefs.has(INSERTION_PREF_ROOT + id);
+function saveLocation(id, toolbarId, nextItemId) {
+  let _toolbarId = toolbarId || "";
+  let _nextItemId = nextItemId || "";
+  prefs.set(LOCATION_PREF_ROOT + id, [_toolbarId, _nextItemId].join(","));
 }
 
+// Insert button into window
 function insertButton(aWindow, id, onBuild) {
   // Build button and save reference to it
   let doc = aWindow.document;
@@ -37,58 +45,54 @@ function insertButton(aWindow, id, onBuild) {
   let toolbox = doc.getElementById("navigator-toolbox");
   toolbox.palette.appendChild(b);
 
-  // Search for widget toolbar by reading toolbar's currentset attribute
-  let container = null;
-  let toolbars = doc.getElementsByTagName("toolbar");
-  for (let i = 0, l = toolbars.length; i < l; i++) {
-    let toolbar = toolbars[i];
-    if (toolbar.getAttribute("currentset").indexOf(id) == -1)
-      continue;
-    container = toolbar;
-  }
+  // Retrieve button location from preferences
+  let [toolbarId, nextItemId] = getLocation(id);
+  let toolbar = toolbarId != "" && doc.getElementById(toolbarId);
 
-  // if widget isn't in any toolbar, add it to the nav-bar
-  if (!container) {
-    if (haveInserted(id)) {
-      return;
+  if (toolbar) {
+    let nextItem = doc.getElementById(nextItemId);
+    // If nextItem not in toolbar then retrieve it by reading currentset attribute
+    if (!(nextItem && nextItem.parentNode && nextItem.parentNode.id == toolbarId)) {
+      nextItem = null;
+      let currentSet = toolbar.getAttribute("currentset");
+      let ids = (currentSet == "__empty") ? [] : currentSet.split(",");
+      let idx = ids.indexOf(id);
+      if (idx != -1) {
+        for (let i = idx; i < ids.length; i++) {
+          nextItem = doc.getElementById(ids[i]);
+          if (nextItem)
+            break;
+        }
+      }
     }
-    container = doc.getElementById("nav-bar");
-    saveInserted(id);
-  }
-
-  // Now retrieve a reference to the next toolbar item
-  // by reading currentset attribute on the toolbar
-  let nextNode = null;
-  let currentSet = container.getAttribute("currentset");
-  let ids = (currentSet == "__empty") ? [] : currentSet.split(",");
-  let idx = ids.indexOf(id);
-  if (idx != -1) {
-    for (let i = idx; i < ids.length; i++) {
-      nextNode = doc.getElementById(ids[i]);
-      if (nextNode)
-        break;
-    }
-  }
-
-  // Finally insert our widget in the right toolbar and in the right position
-  container.insertItem(id, nextNode, null, false);
-
-  // Update DOM in order to save position: which toolbar, and which position
-  // in this toolbar. But only do this the first time we add it to the toolbar
-  // Otherwise, this code may collide with other instance of Widget module
-  // during Firefox startup. See bug 685929.
-  if (ids.indexOf(id) == -1) {
-    let set = container.currentSet;
-    container.setAttribute("currentset", set);
-    // Save DOM attribute in order to save position on new window opened
-    aWindow.document.persist(container.id, "currentset");
+    // Finally insert button in the right toolbar and in the right position
+    toolbar.insertItem(id, nextItem, null, false);
   }
 }
 
+// Remove button from window
 function removeButton(aWindow, id) {
   let b = aWindow[id];
   b.parentNode.removeChild(b);
   delete aWindow[id];
+}
+
+// Save locations of buttons after customization
+function afterCustomize(e) {
+  for (let [id] of buttonsList) {
+    let toolbox = e.target;
+    let b = toolbox.parentNode.querySelector("#" + id);
+    let toolbarId = null, nextItemId = null;
+    if (b) {
+      let parent = b.parentNode;
+      let nextItem = b.nextSibling;
+      if (parent && parent.localName == "toolbar") {
+        toolbarId = parent.id;
+        nextItemId = nextItem && nextItem.id;
+      }
+    }
+    saveLocation(id, toolbarId, nextItemId);
+  }
 }
 
 // Global window observer
@@ -119,23 +123,25 @@ browserWindowObserver.prototype = {
 
 // Run on every window startup
 function browserWindowStartup(aWindow) {
-  for (let [id, onBuild] of blist) {
+  for (let [id, onBuild] of buttonsList) {
     insertButton(aWindow, id, onBuild);
   }
+  aWindow.addEventListener("aftercustomization", afterCustomize, false);
 };
 
 // Run on every window shutdown
 function browserWindowShutdown(aWindow) {
-  for (let [id, onBuild] of blist) {
+  for (let [id, onBuild] of buttonsList) {
     removeButton(aWindow, id);
   }
+  aWindow.removeEventListener("aftercustomization", afterCustomize, false);
 }
 
 // Main object
 const buttons = {
   createButton: function(aProperties) {
-    // If no buttons were inserted, setup global window observer
-    if (blist.size == 0) {
+    // If no buttons were inserted yet, setup global window observer
+    if (buttonsList.size == 0) {
       let ww = Cc["@mozilla.org/embedcomp/window-watcher;1"].getService(Ci.nsIWindowWatcher);
       gWindowListener = new browserWindowObserver({
         onStartup: browserWindowStartup,
@@ -144,21 +150,28 @@ const buttons = {
       ww.registerNotification(gWindowListener);
     }
 
-    // Add button to list and insert to all open windows
-    blist.set(aProperties.id, aProperties.onBuild);
+    // Add button to list
+    buttonsList.set(aProperties.id, aProperties.onBuild);
+
+    // Inster button to all open windows
     let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
     let winenu = wm.getEnumerator("navigator:browser");
     while (winenu.hasMoreElements()) {
-      insertButton(winenu.getNext(), aProperties.id, aProperties.onBuild);
+      let win = winenu.getNext();
+      insertButton(win, aProperties.id, aProperties.onBuild);
+      // When first button inserted, add afterCustomize listener
+      if (buttonsList.size == 1) {
+        win.addEventListener("aftercustomization", afterCustomize, false);
+      }
     }
   },
 
   destroyButton: function(id) {
     // Remove button from list
-    blist.delete(id);
+    buttonsList.delete(id);
 
-    // If list is empty, remove global window observer
-    if (blist.size == 0) {
+    // If no more buttons exist, remove global window observer
+    if (buttonsList.size == 0) {
       let ww = Cc["@mozilla.org/embedcomp/window-watcher;1"].getService(Ci.nsIWindowWatcher);
       ww.unregisterNotification(gWindowListener);
       gWindowListener = null;
@@ -168,7 +181,12 @@ const buttons = {
     let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
     let winenu = wm.getEnumerator("navigator:browser");
     while (winenu.hasMoreElements()) {
-      removeButton(winenu.getNext(), id);
+      let win = winenu.getNext();
+      removeButton(win, id);
+      // If no more buttons exist, remove afterCustomize listener
+      if (buttonsList.size == 0) {
+        win.removeEventListener("aftercustomization", afterCustomize, false);
+      }
     }
   },
 

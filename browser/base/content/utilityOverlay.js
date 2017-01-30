56c725cca9292cd95999e8b5c86c7950d5dc02cb
Pale Moon <git-repo@palemoon.org>
2017-01-30 01:41:06 +0100

    Remove unused URI_IS_UTF8 load flag.
    This resolves #865.

--- browser/base/content/utilityOverlay.js
+++ browser/base/content/utilityOverlay.js
@@ -217,8 +217,6 @@ function openLinkIn(url, where, params) {
   var aRelatedToCurrent     = params.relatedToCurrent;
   var aInBackground         = params.inBackground;
   var aDisallowInheritPrincipal = params.disallowInheritPrincipal;
-  // Currently, this parameter works only for where=="tab" or "current"
-  var aIsUTF8               = params.isUTF8;
   var aInitiatingDoc        = params.initiatingDoc;
   var aIsPrivate            = params.private;
 
@@ -307,8 +305,6 @@ function openLinkIn(url, where, params) {
       flags |= Ci.nsIWebNavigation.LOAD_FLAGS_ALLOW_THIRD_PARTY_FIXUP;
     if (aDisallowInheritPrincipal)
       flags |= Ci.nsIWebNavigation.LOAD_FLAGS_DISALLOW_INHERIT_OWNER;
-    if (aIsUTF8)
-      flags |= Ci.nsIWebNavigation.LOAD_FLAGS_URI_IS_UTF8;
     w.gBrowser.loadURIWithFlags(url, flags, aReferrerURI, null, aPostData);
     break;
   case "tabshifted":
@@ -322,8 +318,7 @@ function openLinkIn(url, where, params) {
                        postData: aPostData,
                        inBackground: loadInBackground,
                        allowThirdPartyFixup: aAllowThirdPartyFixup,
-                       relatedToCurrent: aRelatedToCurrent,
-                       isUTF8: aIsUTF8});
+                       relatedToCurrent: aRelatedToCurrent});
     break;
   }
 

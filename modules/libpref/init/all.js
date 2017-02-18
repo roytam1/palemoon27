bba94b1910268ece8bac4aea61d7133a8d9cb46e
wsdfhjxc <wsdfhjxc@protonmail.com>
2017-02-18 17:07:41 +0100

    Add a pref to highlight all findings by default

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -753,6 +753,7 @@ pref("accessibility.typeaheadfind.autostart", true);
 //     1 - "always" (case-sensitive)
 // other - "auto"   (case-sensitive for mixed-case input, insensitive otherwise)
 pref("accessibility.typeaheadfind.casesensitive", 0);
+pref("accessibility.typeaheadfind.highlightallbydefault", true);
 pref("accessibility.typeaheadfind.linksonly", true);
 pref("accessibility.typeaheadfind.startlinksonly", false);
 pref("accessibility.typeaheadfind.timeout", 4000);

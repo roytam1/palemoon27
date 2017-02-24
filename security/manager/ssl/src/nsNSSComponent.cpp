5546cc421e1731315dd95412ba1b84d522bfe8d2
Pale Moon <git-repo@palemoon.org>
2017-02-24 21:50:42 +0100

    Temporarily disable Camellia-GCM suites in secmanager.

--- security/manager/ssl/src/nsNSSComponent.cpp
+++ security/manager/ssl/src/nsNSSComponent.cpp
@@ -639,11 +639,6 @@ static const CipherPref sCipherPrefs[] = {
  { "security.ssl3.ecdhe_ecdsa_aes_128_gcm_sha256",
    TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256, true },
    
- { "security.ssl3.ecdhe_rsa_camellia_128_gcm_sha256",
-   TLS_ECDHE_RSA_WITH_CAMELLIA_128_GCM_SHA256, true },
- { "security.ssl3.ecdhe_ecdsa_camellia_128_gcm_sha256",
-   TLS_ECDHE_ECDSA_WITH_CAMELLIA_128_GCM_SHA256, true },
-
  { "security.ssl3.ecdhe_rsa_aes_256_sha",
    TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA, true },
  { "security.ssl3.ecdhe_ecdsa_aes_256_sha",

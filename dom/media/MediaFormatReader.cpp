bcbd26d984dba70f9e646a782b4f3b98c90cae06
trav90 <travawine@protonmail.ch>
2017-03-01 04:06:40 -0600

    Ensure we have up to date buffered time ranges when OnTaskQueue

--- dom/media/MediaFormatReader.cpp
+++ dom/media/MediaFormatReader.cpp
@@ -1240,6 +1240,15 @@ MediaFormatReader::GetBuffered()
     }
     intervals = mCachedTimeRanges;
   } else {
+    if (OnTaskQueue()) {
+      // Ensure we have up to date buffered time range.
+      if (HasVideo()) {
+        UpdateReceivedNewData(TrackType::kVideoTrack);
+      }
+      if (HasAudio()) {
+        UpdateReceivedNewData(TrackType::kAudioTrack);
+      }
+    }
     if (HasVideo()) {
       MonitorAutoLock lock(mVideo.mMonitor);
       videoti = mVideo.mTimeRanges;

99a8fc2898e66384c30d2cecbe5474b7e089a6fc
Pale Moon <git-repo@palemoon.org>
2017-03-14 10:30:54 +0100

    Make DOM selection-addRange spec compliant.

--- layout/generic/nsSelection.cpp
+++ layout/generic/nsSelection.cpp
@@ -4632,6 +4632,16 @@ Selection::AddRange(nsIDOMRange* aDOMRange)
 void
 Selection::AddRange(nsRange& aRange, ErrorResult& aRv)
 {
+  auto rangeRoot = aRange.GetRoot();
+  auto doc = GetParentObject();
+  if (doc != rangeRoot && (!rangeRoot ||
+                           doc != rangeRoot->GetComposedDoc())) {
+    // http://w3c.github.io/selection-api/#dom-selection-addrange
+    // "...  if the root of the range's boundary points are the document
+    // associated with context object. Otherwise, this method must do nothing."
+    return;
+  }
+
   // This inserts a table cell range in proper document order
   // and returns NS_OK if range doesn't contain just one table cell
   bool didAddRange;

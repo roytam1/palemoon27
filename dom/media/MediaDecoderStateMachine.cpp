176afffe581d1974cda7da19e2c2cad96f0815ea
trav90 <travawine@protonmail.com>
2017-04-10 04:41:43 -0500

    Invoke SeekCompleted synchronously
    The current behavior is a holdover from when this some of this stuff was happening on the decode thread. It's problematic because nothing guarantees that we don't call CheckIfSeekComplete twice, and queue up two SeekCompleted events.

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -1062,6 +1062,7 @@ MediaDecoderStateMachine::CheckIfSeekComplete()
 {
   MOZ_ASSERT(OnTaskQueue());
   AssertCurrentThreadInMonitor();
+  MOZ_ASSERT(mState == DECODER_STATE_SEEKING);
 
   const bool videoSeekComplete = IsVideoSeekComplete();
   if (HasVideo() && !videoSeekComplete) {
@@ -1086,9 +1087,7 @@ MediaDecoderStateMachine::CheckIfSeekComplete()
 
   if (audioSeekComplete && videoSeekComplete) {
     mDecodeToSeekTarget = false;
-    nsCOMPtr<nsIRunnable> task(
-      NS_NewRunnableMethod(this, &MediaDecoderStateMachine::SeekCompleted));
-    TaskQueue()->Dispatch(task.forget());
+    SeekCompleted();
   }
 }
 
@@ -2369,15 +2368,7 @@ MediaDecoderStateMachine::SeekCompleted()
 {
   MOZ_ASSERT(OnTaskQueue());
   ReentrantMonitorAutoEnter mon(mDecoder->GetReentrantMonitor());
-
-  if (mState != DECODER_STATE_SEEKING) {
-    MOZ_DIAGNOSTIC_ASSERT(mState == DECODER_STATE_DORMANT ||
-                          IsShutdown());
-    // It would be nice to assert mCurrent.Exists() here, but it's possible that
-    // we've transitioned to DECODER_STATE_SHUTDOWN but not yet gone through
-    // RunStateMachine in that state, which is where this promise gets rejected.
-    return;
-  }
+  MOZ_ASSERT(mState == DECODER_STATE_SEEKING);
 
   int64_t seekTime = mCurrentSeek.mTarget.mTime;
   int64_t newCurrentTime = seekTime;

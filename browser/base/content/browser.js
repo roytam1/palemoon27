8846d3466b142fe62962565cf5f389dc5f8b92b8
Pale Moon <git-repo@palemoon.org>
2017-04-21 15:41:00 +0200

    Properly handle displayed address if URL changes.

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -2117,8 +2117,13 @@ function URLBarSetURI(aURI) {
     valid = !isBlankPageURL(uri.spec);
   }
 
+  let isDifferentValidValue = valid && value != gURLBar.value;
   gURLBar.value = value;
   gURLBar.valueIsTyped = !valid;
+  if (isDifferentValidValue) {
+    gURLBar.selectionStart = gURLBar.selectionEnd = 0;
+  }
+
   SetPageProxyState(valid ? "valid" : "invalid");
 }
 

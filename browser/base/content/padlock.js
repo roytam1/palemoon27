941e8466ffc386c1e9ca7c55f37dfada77e61724
JustOff <Off.Just.Off@gmail.com>
2017-04-28 21:30:10 +0300

    Add patch description

--- browser/base/content/padlock.js
+++ browser/base/content/padlock.js
@@ -122,6 +122,9 @@ var padlock_PadLock =
   },
   prefbranch : null,
   onLoad: function() {
+    // gBrowser should be always defined at this point, but if it is not then most likely 
+    // it is due to incompatible or outdated language pack is installed and selected.
+    // In this case we reset "general.useragent.locale" to try to recover browser startup.
     if (typeof gBrowser === "undefined") {
       var prefBranch = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefBranch);
       prefBranch.clearUserPref("general.useragent.locale");

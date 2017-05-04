0136f827ca2f357eb41fb45eaa7884189a436875
Pale Moon <git-repo@palemoon.org>
2017-05-04 02:09:29 +0200

    Remove style rule processing for XP themes.
    XP specific themes Luna Blue/Olive/Silver and Royale+Zune removed.

--- widget/LookAndFeel.h
+++ widget/LookAndFeel.h
@@ -407,11 +407,6 @@ public:
     eWindowsTheme_Generic = 0, // unrecognized theme
     eWindowsTheme_Classic,
     eWindowsTheme_Aero,
-    eWindowsTheme_LunaBlue,
-    eWindowsTheme_LunaOlive,
-    eWindowsTheme_LunaSilver,
-    eWindowsTheme_Royale,
-    eWindowsTheme_Zune,
     eWindowsTheme_AeroLite
   };
 

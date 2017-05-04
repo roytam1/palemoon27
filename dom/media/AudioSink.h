8274def9b49941a74f25dd317f21580e9def95d6
trav90 <travawine@protonmail.com>
2017-05-04 00:08:51 -0500

    Re-land "Fix data race in AudioSink::mWritten"

--- dom/media/AudioSink.h
+++ dom/media/AudioSink.h
@@ -9,6 +9,7 @@
 #include "nsISupportsImpl.h"
 #include "MediaDecoderReader.h"
 #include "mozilla/dom/AudioChannelBinding.h"
+#include "mozilla/Atomics.h"
 
 namespace mozilla {
 
@@ -118,7 +119,7 @@ private:
   int64_t mStartTime;
 
   // PCM frames written to the stream so far.
-  int64_t mWritten;
+  Atomic<int64_t> mWritten;
 
   // Keep the last good position returned from the audio stream. Used to ensure
   // position returned by GetPosition() is mono-increasing in spite of audio

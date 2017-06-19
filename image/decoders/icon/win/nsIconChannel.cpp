611039d975017fe982bfb1fdb1b795840911c557
Pale Moon <git-repo@palemoon.org>
2017-06-19 14:46:02 +0200

    Replace correct #define check

--- image/decoders/icon/win/nsIconChannel.cpp
+++ image/decoders/icon/win/nsIconChannel.cpp
@@ -25,7 +25,9 @@
 #include "nsDirectoryServiceDefs.h"
 #include "nsProxyRelease.h"
 
+#ifdef _WIN32_WINNT
 #undef _WIN32_WINNT
+#endif
 #define _WIN32_WINNT 0x0600
 
 // we need windows.h to read out registry information...

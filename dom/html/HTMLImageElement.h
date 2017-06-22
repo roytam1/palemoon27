16f13e5053ad431ef9dd4609c57a6d1f8989c0db
janekptacijarabaci <janekptacijarabaci@seznam.cz>
2017-06-22 09:02:59 +0200

    HTML - picture - does not react to resize/viewport changes

--- dom/html/HTMLImageElement.h
+++ dom/html/HTMLImageElement.h
@@ -205,6 +205,8 @@ public:
 
   virtual void DestroyContent() override;
 
+  void MediaFeatureValuesChanged();
+
   /**
    * Given a hypothetical <img> or <source> tag with the given parameters,
    * return what URI we would attempt to use, if any.  Used by the preloader to
@@ -331,6 +333,8 @@ protected:
   nsRefPtr<ResponsiveImageSelector> mResponsiveSelector;
 
 private:
+  bool SourceElementMatches(nsIContent* aSourceNode);
+
   static void MapAttributesIntoRule(const nsMappedAttributes* aAttributes,
                                     nsRuleData* aData);
 

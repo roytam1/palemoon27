38c1e679510c2d8e3876b88939a53beb415c7f5a
trav90 <travawine@protonmail.com>
2017-07-02 04:39:07 -0500

    Move post-duration-change seeking into MediaDecoder where it belongs

--- dom/media/MediaDecoder.cpp
+++ dom/media/MediaDecoder.cpp
@@ -1375,6 +1375,10 @@ void MediaDecoder::DurationChanged(TimeUnit aNewDuration)
     DECODER_LOG("Duration changed to %lld", mDuration);
     mOwner->DispatchAsyncEvent(NS_LITERAL_STRING("durationchange"));
   }
+
+  if (CurrentPosition() > aNewDuration.ToMicroseconds()) {
+    Seek(aNewDuration.ToSeconds(), SeekTarget::Accurate);
+  }
 }
 
 void MediaDecoder::SetNetworkDuration(TimeUnit aNetworkDuration)

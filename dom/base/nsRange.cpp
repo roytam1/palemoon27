b1b6928df356f770fd9eb12e515b499e3b91e457
janekptacijarabaci <janekptacijarabaci@seznam.cz>
2017-07-05 01:18:57 +0200

    DOM - implement selection events (potentially crash)

--- dom/base/nsRange.cpp
+++ dom/base/nsRange.cpp
@@ -3079,7 +3079,7 @@ nsRange::AutoInvalidateSelection::~AutoInvalidateSelection()
   mIsNested = false;
   ::InvalidateAllFrames(mCommonAncestor);
   nsINode* commonAncestor = mRange->GetRegisteredCommonAncestor();
-  if (commonAncestor != mCommonAncestor) {
+  if (commonAncestor && commonAncestor != mCommonAncestor) {
     ::InvalidateAllFrames(commonAncestor);
   }
 }

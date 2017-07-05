ff5bceddf75e2b3f8a39d6c036042cb151206c13
trav90 <travawine@protonmail.ch>
2017-07-05 02:37:45 -0500

    Distinguish unknown track types from errors in libnestegg
    Cherry-picked from upstream.

--- media/libnestegg/src/nestegg.c
+++ media/libnestegg/src/nestegg.c
@@ -2168,7 +2168,7 @@ nestegg_track_type(nestegg * ctx, unsigned int track)
   if (type & TRACK_TYPE_AUDIO)
     return NESTEGG_TRACK_AUDIO;
 
-  return -1;
+  return NESTEGG_TRACK_UNKNOWN;
 }
 
 int
@@ -2196,7 +2196,7 @@ nestegg_track_codec_id(nestegg * ctx, unsigned int track)
   if (strcmp(codec_id, TRACK_ID_OPUS) == 0)
     return NESTEGG_CODEC_OPUS;
 
-  return -1;
+  return NESTEGG_CODEC_UNKNOWN;
 }
 
 int

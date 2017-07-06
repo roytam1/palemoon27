ee8b2734a973be272ad61809f956b1a1cb944574
John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
2017-07-06 13:51:51 +0200

    media:webrtc: Use better pre-processor defines for sparc64

--- media/webrtc/trunk/build/build_config.h
+++ media/webrtc/trunk/build/build_config.h
@@ -134,7 +134,7 @@
 #define ARCH_CPU_M68K 1
 #define ARCH_CPU_32_BITS 1
 #define ARCH_CPU_BIG_ENDIAN 1
-#elif defined(__sparc64__)
+#elif defined(__sparc__) && defined(__arch64__)
 #define ARCH_CPU_SPARC_FAMILY 1
 #define ARCH_CPU_SPARC 1
 #define ARCH_CPU_64_BITS 1

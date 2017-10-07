dfe9983baff5f74f744b7a1dde697598c565be70
janekptacijarabaci <janekptacijarabaci@seznam.cz>
2017-10-07 09:03:10 +0200

    [UXP task] gre/modules/commonjs/sdk/lang/weak-set.js
    JavaScript strict warning:
    resource://gre/modules/commonjs/sdk/lang/weak-set.js, line 9:
    SyntaxError: 'use strict' statement won't be enforced as a directive
    because it isn't in directive prologue position

--- toolkit/commonjs/sdk/lang/weak-set.js
+++ toolkit/commonjs/sdk/lang/weak-set.js
@@ -2,11 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+"use strict";
+
 module.metadata = {
   "stability": "experimental"
 };
-
-"use strict";
 
 const { Cu } = require("chrome");
 

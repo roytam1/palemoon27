ddfb42b23934e53c4564b3aee8e832b4d32e446b
Pale Moon <git-repo@palemoon.org>
2017-10-24 20:00:16 +0200

    Don't set a fallback detailsURL since we supply this in the update XML.

--- browser/branding/unstable/pref/palemoon-branding.js
+++ browser/branding/unstable/pref/palemoon-branding.js
@@ -25,10 +25,6 @@ pref("app.update.promptWaitTime", 86400);
 // URL user can browse to manually if for some reason all update installation
 // attempts fail.
 pref("app.update.url.manual", "http://www.palemoon.org/unstable/");
-
-// A default value for the "More information about this update" link
-// supplied in the "An update is available" page of the update wizard. 
-pref("app.update.url.details", "http://www.palemoon.org/unstable/");
 #else
 // Updates disabled (Linux, etc.)
 pref("app.update.enabled", false);

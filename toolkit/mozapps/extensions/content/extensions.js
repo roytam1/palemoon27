52e5e9258191bae1729612212ab80219a6a01b03
JustOff <Off.Just.Off@gmail.com>
2017-11-01 18:49:56 +0200

    Сode style adjustment

--- toolkit/mozapps/extensions/content/extensions.js
+++ toolkit/mozapps/extensions/content/extensions.js
@@ -1651,7 +1651,7 @@ function doPendingUninstalls(aListBox) {
   var listitem = aListBox.firstChild;
   while (listitem) {
     if (listitem.getAttribute("pending") == "uninstall" &&
-        !(listitem.opRequiresRestart("UNINSTALL")))
+        !(listitem.opRequiresRestart("uninstall")))
       items.push(listitem.mAddon);
     listitem = listitem.nextSibling;
   }

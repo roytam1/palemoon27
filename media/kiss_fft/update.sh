1e3b592c4733930b4ad1bd64015117f59217ef97
Pale Moon <git-repo@palemoon.org>
2018-01-22 11:01:20 +0100

    Update kiss-fft to 1.4.0
    This updates it to our own fork of kiss-fft that fixes a number of parallelization issues.
    We keep -openmp disabled for now because there are still issues with using it in parallel mode.

--- media/kiss_fft/update.sh
+++ media/kiss_fft/update.sh
@@ -6,10 +6,11 @@ if [ ! -d "$1" ]; then
   exit 1
 fi
 
-FILES="CHANGELOG \
-       COPYING \
+FILES="docs/CHANGELOG \
+       LICENSE \
        README \
-       README.simd \
+       docs/README.simd \
+       docs/BACKGROUND \
        _kiss_fft_guts.h \
        kiss_fft.c \
        kiss_fft.h \
@@ -20,15 +21,5 @@ for file in $FILES; do
   cp "$1/$file" .
 done
 
-if [ -d "$1/.hg" ]; then
-  rev=$(cd "$1" && hg log --template='{node}' -r `hg identify -i`)
-fi
-
-if [ -n "$rev" ]; then
-  version=$rev
-  sed -i.bak -e "/The hg revision ID used was/ s/[0-9a-f]\{40\}\./$version./" README_MOZILLA
-  rm README_MOZILLA.bak
-else
-  echo "Remember to update README_MOZILLA with the version details."
-fi
+echo "Remember to update README_MOZILLA with the version details."
 

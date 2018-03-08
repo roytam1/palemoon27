cde5a54751d12845a4f9a43e346714c2e463a6e0
Pale Moon <git-repo@palemoon.org>
2018-03-08 15:29:01 +0100

    Switch to the Open Twemoji font resources.
    This deprecates EmojiOne in favor of Twemoji which doesn't have a restrictive license.

--- gfx/thebes/gfxWindowsPlatform.cpp
+++ gfx/thebes/gfxWindowsPlatform.cpp
@@ -837,7 +837,6 @@ static const char kFontCambriaMath[] = "Cambria Math";
 static const char kFontEbrima[] = "Ebrima";
 static const char kFontEstrangeloEdessa[] = "Estrangelo Edessa";
 static const char kFontEuphemia[] = "Euphemia";
-static const char kFontEmojiOneMozilla[] = "EmojiOne Mozilla";
 static const char kFontGabriola[] = "Gabriola";
 static const char kFontJavaneseText[] = "Javanese Text";
 static const char kFontKhmerUI[] = "Khmer UI";
@@ -864,6 +863,7 @@ static const char kFontSegoeUIEmoji[] = "Segoe UI Emoji";
 static const char kFontSegoeUISymbol[] = "Segoe UI Symbol";
 static const char kFontSylfaen[] = "Sylfaen";
 static const char kFontTraditionalArabic[] = "Traditional Arabic";
+static const char kFontTwemojiMozilla[] = "Twemoji Mozilla";
 static const char kFontUtsaah[] = "Utsaah";
 static const char kFontYuGothic[] = "Yu Gothic";
 
@@ -874,7 +874,7 @@ gfxWindowsPlatform::GetCommonFallbackFonts(uint32_t aCh, uint32_t aNextCh,
 {
     if (aNextCh == 0xfe0fu) {
         aFontList.AppendElement(kFontSegoeUIEmoji);
-        aFontList.AppendElement(kFontEmojiOneMozilla);
+        aFontList.AppendElement(kFontTwemojiMozilla);
     }
 
     // Arial is used as the default fallback for system fallback
@@ -886,11 +886,11 @@ gfxWindowsPlatform::GetCommonFallbackFonts(uint32_t aCh, uint32_t aNextCh,
             if (aNextCh == 0xfe0eu) {
                 aFontList.AppendElement(kFontSegoeUISymbol);
                 aFontList.AppendElement(kFontSegoeUIEmoji);
-                aFontList.AppendElement(kFontEmojiOneMozilla);
+                aFontList.AppendElement(kFontTwemojiMozilla);
             } else {
                 if (aNextCh != 0xfe0fu) {
                     aFontList.AppendElement(kFontSegoeUIEmoji);
-                    aFontList.AppendElement(kFontEmojiOneMozilla);
+                    aFontList.AppendElement(kFontTwemojiMozilla);
                 }
                 aFontList.AppendElement(kFontSegoeUISymbol);
             }

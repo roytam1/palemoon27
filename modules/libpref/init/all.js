6d546ecefb24841907463ac1e32ca1aa76766b89
Pale Moon <git-repo@palemoon.org>
2018-04-05 15:46:00 +0200

    Adjust for a specific JPEG-XR encoding error and enable the workaround by default.
    Adds debug code for image check. Remove before release.

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -316,9 +316,8 @@ pref("media.jxr.autoaccept", true);
 // updated with the new type. This pref is mainly for testing and should be
 // removed once the preferred type (most likely "image/jxr") has been chosen.
 pref("media.jxr.advertised_mime_type", "image/jxr");
-// Work around the JXELIB encoding bug for incorrect bytecounts.
-// Enabling this -may- lead to crashes in rare circumstances.
-pref("media.jxr.workaround_alphaplane_bug", false);
+// Work around a JPEG-XR encoding bug for incorrect Alpha Plane bytecounts.
+pref("media.jxr.workaround_alphaplane_bug", true);
 #endif
 #ifdef MOZ_DIRECTSHOW
 pref("media.directshow.enabled", true);

259ab989474cbe48ee37c9505c22be2cb6a189b9
Roy Tam <roytam@gmail.com>
2018-07-12 12:15:49 +0800

    applied mybrowser-20171029.diff

--- browser/branding/unofficial/pref/palemoon-branding.js
+++ browser/branding/unofficial/pref/palemoon-branding.js
@@ -4,7 +4,10 @@
 #include ../../shared/pref/uaoverrides.inc
 
 pref("startup.homepage_override_url","http://www.palemoon.org/unofficial.shtml");
-pref("app.releaseNotesURL", "http://www.palemoon.org/releasenotes.shtml");
+pref("app.releaseNotesURL", "https://github.com/Feodor2/MyPal");
+
+// Enable Firefox compatmode by default.
+pref("general.useragent.compatMode", 2);
 
 // Updates disabled
 pref("app.update.enabled", false);

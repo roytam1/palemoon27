f32ba41c0d983548097dabee3858697258f70a01
Roy Tam <roytam@gmail.com>
2018-07-12 12:21:54 +0800

    applied libvpx-fx52.diff

--- media/libvpx/update.py
+++ media/libvpx/update.py
@@ -398,14 +398,13 @@ def prepare_upstream(prefix, commit=None):
         configure = ['../../configure', '--target=%s' % target,
             '--disable-examples', '--disable-install-docs',
             '--enable-multi-res-encoding',
-            '--size-limit=4000x3000'
+            '--size-limit=8192x4608'
         ]
 
         if 'darwin9' in target:
             configure += ['--enable-pic']
         if 'linux' in target:
             configure += ['--enable-pic']
-            configure += ['--disable-avx2']
         # x86inc.asm is not compatible with pic 32bit builds
         if target == 'x86-linux-gcc':
             configure += ['--disable-use-x86inc']
@@ -593,6 +592,22 @@ def apply_patches():
     # Cherry pick https://chromium-review.googlesource.com/#/c/276889/
     # to fix crash on 32bit
     os.system("patch -p1 < vp9_filter_restore_aligment.patch")
+    # Patch win32 vpx_once.
+    os.system("patch -p3 < vpx_once.patch")
+    # Bug 1224363 - Clamp seg_lvl also in abs-value mode.
+    os.system("patch -p3 < clamp_abs_lvl_seg.patch")
+    # Bug 1224361 - Clamp QIndex also in abs-value mode.
+    os.system("patch -p3 < clamp-abs-QIndex.patch")
+    # Bug 1233983 - Make libvpx build with clang-cl
+    os.system("patch -p3 < clang-cl.patch")
+    # Bug 1224371 - Cast uint8_t to uint32_t before shift
+    os.system("patch -p3 < cast-char-to-uint-before-shift.patch")
+    # Bug 1237848 - Check lookahead ctx
+    os.system("patch -p3 < 1237848-check-lookahead-ctx.patch")
+    # Bug 1263384 - Check input frame resolution
+    os.system("patch -p3 < input_frame_validation.patch")
+    # Bug 1315288 - Check input frame resolution for vp9
+    os.system("patch -p3 < input_frame_validation_vp9.patch")
 
 def update_readme(commit):
     with open('README_MOZILLA') as f:

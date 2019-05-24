8e93515696f8b371d6142a7ab691ca8bc0344e33
Roy Tam <roytam@gmail.com>
2019-05-24 21:31:04 +0800

    import change from rmottola/Arctic-Fox: - Bug 1143256 - Store object metadata using a weak map (38a6dc237) - Bug 1143513 - Make nsFrameList compatible with range-based syntax and utils. (2c69cf891) -  Bug 1149854: Use range-based for loops when iterating over child frames in nsFlexContainerFrame.cpp. (5c1e258db)

--- layout/generic/nsIFrame.h
+++ layout/generic/nsIFrame.h
@@ -3403,6 +3403,27 @@ nsFrameList::FrameLinkEnumerator::Next()
   Enumerator::Next();
 }
 
+// Operators of nsFrameList::Iterator
+// ---------------------------------------------------
+
+inline nsFrameList::Iterator&
+nsFrameList::Iterator::operator++()
+{
+  mCurrent = mCurrent->GetNextSibling();
+  return *this;
+}
+
+inline nsFrameList::Iterator&
+nsFrameList::Iterator::operator--()
+{
+  if (!mCurrent) {
+    mCurrent = mList.LastChild();
+  } else {
+    mCurrent = mCurrent->GetPrevSibling();
+  }
+  return *this;
+}
+
 // Helper-functions for nsIFrame::SortFrameList()
 // ---------------------------------------------------
 

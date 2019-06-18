a6abc84e91e91b641ddbedd2d750997a482df01a
Roy Tam <roytam@gmail.com>
2019-06-18 19:45:21 +0800

    import change from rmottola/Arctic-Fox: - Bug 1141392 - Disallow a line break between ArrowParameters and the arrow. (386dac894) - Bug 1138651 - Enable CSS Scroll Snapping by Default on B2G. (2e416be93) - Bug 1140623 - Correct mochitest failures that occur when the layout.css.scroll-snap.enabled preference is enabled (V3 Patch). (cfb1ccd33) - Bug 1136467 - ServiceWorker: client.postMessage should land in navigator.serviceWorker.onmessage. (547bc9d03) - Bug 1137419 - Use correct principal for update. (9b409bd5c) - Bug 1137408 - Use inner window to create ServiceWorker for Fetch Event (11958cf2b) -  Bug 1137250 - Fix incorrect initialization of Request object. (ec637ee57) -  Bug 1139990 - Remove No activatingWorker warning. (49154e967) - Bug 1058311 - Update client interface. Implement matchAll WindowClient. (cde7ba62a) - Bug 1131353 - Make ServiceWorkerGlobalScope.close() throw InvalidAccessError; (0ec3f45d9) - Bug 1137816 - Add a test for interfaces exposed to service workers; (19b5f9504) - Bug 1137816 follow-up: Add WindowClient, added by bug 1058311 (ce6f0ba73) - Bug 1141274 - Allocate shared workers and service workers from separate namespaces; (776221d45) - Bug 1133763 - Part 1: Remember the security info associated with HTTP fetches and record it inside InternalResponse; (fec70ad02) - Bug 1133763 - Part 2: Transfer the security info associated with an InternalResponse across the IPC layer (e7192f129) -  Bug 1133763 - Part 3: Wipe out the cache directory when detecting a change in the DB schema; (82d81819a) - Bug 1133763 - Part 4: Store the response's security info in the cache database; (426e94e4b) - Bug 1133763 - Part 5: Allow the security info on intercepted HTTP channels to be overridden (4d079bf92) - Bug 1133763 - Part 6: Set the correct security info on intercepted channels when using the respondWith API (83e2b2a7d) - Bug 1133763 - Part 7: Add automated tests for using FetchEvent.respondWith on an HTTPS request (e0ee10e17) - Bug 1133763 - Part 8: Ensure that FetchEvent.respondWith works correctly on HTTPS requests with a cloned response (0db13351e) - Bug 1139425 - Service Worker Client uuid tests. (480b262ed) - Bug 1139425 - Service Worker Client id should return a DOMString uuid. (8b9ea176d) - Bug 1142853 - Add caret resources to package-manifest.in. (7e1a59681)

--- dom/workers/ServiceWorkerEvents.cpp
+++ dom/workers/ServiceWorkerEvents.cpp
@@ -14,6 +14,7 @@
 #include "nsServiceManagerUtils.h"
 #include "nsStreamUtils.h"
 #include "nsNetCID.h"
+#include "nsSerializationHelper.h"
 
 #include "mozilla/dom/FetchEventBinding.h"
 #include "mozilla/dom/PromiseNativeHandler.h"
@@ -28,7 +29,6 @@ BEGIN_WORKERS_NAMESPACE
 
 FetchEvent::FetchEvent(EventTarget* aOwner)
 : Event(aOwner, nullptr, nullptr)
-, mWindowId(0)
 , mIsReload(false)
 , mWaitToRespond(false)
 {
@@ -41,11 +41,11 @@ FetchEvent::~FetchEvent()
 void
 FetchEvent::PostInit(nsMainThreadPtrHandle<nsIInterceptedChannel>& aChannel,
                      nsMainThreadPtrHandle<ServiceWorker>& aServiceWorker,
-                     uint64_t aWindowId)
+                     nsAutoPtr<ServiceWorkerClientInfo>& aClientInfo)
 {
   mChannel = aChannel;
   mServiceWorker = aServiceWorker;
-  mWindowId = aWindowId;
+  mClientInfo = aClientInfo;
 }
 
 /*static*/ already_AddRefed<FetchEvent>
@@ -92,9 +92,12 @@ public:
 class FinishResponse final : public nsRunnable
 {
   nsMainThreadPtrHandle<nsIInterceptedChannel> mChannel;
+  nsRefPtr<InternalResponse> mInternalResponse;
 public:
-  explicit FinishResponse(nsMainThreadPtrHandle<nsIInterceptedChannel>& aChannel)
+  FinishResponse(nsMainThreadPtrHandle<nsIInterceptedChannel>& aChannel,
+                 InternalResponse* aInternalResponse)
     : mChannel(aChannel)
+    , mInternalResponse(aInternalResponse)
   {
   }
 
@@ -102,7 +105,17 @@ public:
       Run()
   {
     AssertIsOnMainThread();
-    nsresult rv = mChannel->FinishSynthesizedResponse();
+
+    nsCOMPtr<nsISupports> infoObj;
+    nsresult rv = NS_DeserializeObject(mInternalResponse->GetSecurityInfo(), getter_AddRefs(infoObj));
+    if (NS_SUCCEEDED(rv)) {
+      rv = mChannel->SetSecurityInfo(infoObj);
+      if (NS_WARN_IF(NS_FAILED(rv))) {
+        return rv;
+      }
+    }
+
+    rv = mChannel->FinishSynthesizedResponse();
     NS_WARN_IF_FALSE(NS_SUCCEEDED(rv), "Failed to finish synthesized response");
     return rv;
   }
@@ -130,9 +143,12 @@ public:
 struct RespondWithClosure
 {
   nsMainThreadPtrHandle<nsIInterceptedChannel> mInterceptedChannel;
+  nsRefPtr<InternalResponse> mInternalResponse;
 
-  explicit RespondWithClosure(nsMainThreadPtrHandle<nsIInterceptedChannel>& aChannel)
+  RespondWithClosure(nsMainThreadPtrHandle<nsIInterceptedChannel>& aChannel,
+                     InternalResponse* aInternalResponse)
     : mInterceptedChannel(aChannel)
+    , mInternalResponse(aInternalResponse)
   {
   }
 };
@@ -142,7 +158,7 @@ void RespondWithCopyComplete(void* aClosure, nsresult aStatus)
   nsAutoPtr<RespondWithClosure> data(static_cast<RespondWithClosure*>(aClosure));
   nsCOMPtr<nsIRunnable> event;
   if (NS_SUCCEEDED(aStatus)) {
-    event = new FinishResponse(data->mInterceptedChannel);
+    event = new FinishResponse(data->mInterceptedChannel, data->mInternalResponse);
   } else {
     event = new CancelChannelRunnable(data->mInterceptedChannel);
   }
@@ -187,6 +203,11 @@ RespondWithHandler::ResolvedCallback(JSContext* aCx, JS::Handle<JS::Value> aValu
     return;
   }
 
+  nsRefPtr<InternalResponse> ir = response->GetInternalResponse();
+  if (NS_WARN_IF(!ir)) {
+    return;
+  }
+
   nsCOMPtr<nsIInputStream> body;
   response->GetBody(getter_AddRefs(body));
   if (NS_WARN_IF(!body) || NS_WARN_IF(response->BodyUsed())) {
@@ -200,7 +221,7 @@ RespondWithHandler::ResolvedCallback(JSContext* aCx, JS::Handle<JS::Value> aValu
     return;
   }
 
-  nsAutoPtr<RespondWithClosure> closure(new RespondWithClosure(mInterceptedChannel));
+  nsAutoPtr<RespondWithClosure> closure(new RespondWithClosure(mInterceptedChannel, ir));
 
   nsCOMPtr<nsIEventTarget> stsThread = do_GetService(NS_SOCKETTRANSPORTSERVICE_CONTRACTID, &rv);
   if (NS_WARN_IF(!stsThread)) {
@@ -244,10 +265,14 @@ FetchEvent::RespondWith(Promise& aPromise, ErrorResult& aRv)
 }
 
 already_AddRefed<ServiceWorkerClient>
-FetchEvent::Client()
+FetchEvent::GetClient()
 {
   if (!mClient) {
-    mClient = new ServiceWorkerClient(GetParentObject(), mWindowId);
+    if (!mClientInfo) {
+      return nullptr;
+    }
+
+    mClient = new ServiceWorkerClient(GetParentObject(), *mClientInfo);
   }
   nsRefPtr<ServiceWorkerClient> client = mClient;
   return client.forget();

d918e4f0aabd3a0e71d7d19cce642a025d475f52
Roy Tam <roytam@gmail.com>
2019-09-17 22:02:08 +0800

    import changes from rmottola/Arctic-Fox: - Bug 1135427. Add missing early return, because throwing an exception without returning is weird. r=khuey (a24e8ead8) - Bug 1134598 - Enable the vertical writing-mode reftest directory. r=smontagu (739dd949e) - Bug 1054892 - Fix ccache status not shown after shrinking cache size. r=gps (bc5de7ca0) - Bug 1109007 - Adding htmlimports.enabled pref flag. r=wchen (737b2435a) - Bug 1133818 - Fixed GCC warnings in widget/windows. r=jmathies (2e3c460cb) - Bug 1135308 - add pref for disabling font cache. r=m_kato (ac6ab5e05) - missing parts of Bug 897060 - Display select dropdowns in the parent process. (549dd9952) - missing import of  Bug 938359 - [e10s] Support middle-click scroll (9c27e5242) - Bug 898170 - Skip login manager event handlers in e10s mode (r=felipe) (5e0d47a6a) - Bug 516753 - Forward content context menu events to the parent. r=felipc (201431b20) - Bug 960783 - Support "new out-of-process window" menu item in nightly [adapted] (ffdfb5df6) - Bug 1046761 - e10s context menu should handle preventDefault (r=felipe) (e04256d27)

--- widget/windows/nsLookAndFeel.cpp
+++ widget/windows/nsLookAndFeel.cpp
@@ -605,6 +605,8 @@ GetSysFontInfo(HDC aHDC, LookAndFeel::FontID anID,
     case LookAndFeel::eFont_Tooltips:
       ptrLogFont = &ncm.lfStatusFont;
       break;
+    default:
+      MOZ_CRASH();
     }
     break;
 

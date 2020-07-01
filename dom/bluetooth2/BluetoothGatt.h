f342a3e6583f92f7b741e0a64b29b6901fdd7859
Roy Tam <roytam@gmail.com>
2020-07-01 11:18:34 +0800

    import changes from `dev' branch of rmottola/Arctic-Fox: - Bug 1144410 - Remove finished transitions when a frame transitions away from being display:none. r=birtles (a462476b4) - Bug 1159082 - Rename *Readonly animation interfaces to *ReadOnly. r=smaug (bdb20bd01) - Bug 1134019 - Extract MLS fallback to its own class for reuse - r=jdm (a21fd4a8e) - Bug 1132229: Handle I/O errors in Bluetooth's daemon backend, r=shuang (1a669ca4c) - Bug 1146355: Move Bluetooth legacy interface into sub-directory bluetooth1/, r=joliu (228f25961) - parts missing of Bug 1153796: Share BluetoothInterfaceHelpers.{cpp,h} between Bluetooth v1 and v2, r=btian (731c22c4f) - Bug 1152821: Move dom/bluetooth/bluetooth2/BluetoothCommon.h to dom/bluetooth, r=joliu (4dc2a17c6) - Bug 1152821: Share BluetoothCommon.h between Bluetooth v1 and v2, r=joliu (3d091624e) - Bug 1146355: Add Bluetooth v2 to Bluetooth build scripts, r=shuang (1dc627ef9) - Bug 1139298: Replace type int with BluetoothGattStatus enum for GATT client callbacks. r=btian (ab1b8790d) - Bug 1136514 - Add ReadRemoteRssi in GATT client. f=jocelyn, r=btian, r=mrbkap (8d14427a4) - Bug 1114515 - Part 1: Revise BluetoothGattHALInterface for discovering all services and first characteristic/descriptor. r=btian (144ca4367) - Bug 1114515 - Part 2: Add BluetoothUuid, BluetoothGattId[], BluetoothGattServiceId[] into bluetooth ipc protocol. r=btian (e03eb9e91) - Bug 1137579 - [bluetooth2] Replace constant with |sizeof| in |StringToUuid| function, r=shuang (23167ced8) - Bug 1133655 - Replace |DispatchBluetoothReply| with |DispatchReplySuccess| and |DispatchReplyError|, f=jocelyn, f=jaliu, r=shuang (03b95a69e) - Bug 1137103 - Simplify Bluetooth signal distribution function, r=shuang (00107e42f) - Bug 1114515 - Part 3: Implement GATT client DiscoverServices API. r=btian, r=mrbkap (d1196932e) - Bug 1114515 - Par h4: Implement Service, Characteristic, Descriptor interfaces for GATT client DiscoverServices API. r=btian, r=mrbkap (c0b0dcc41)

--- dom/bluetooth2/BluetoothGatt.h
+++ dom/bluetooth2/BluetoothGatt.h
@@ -11,6 +11,7 @@
 #include "mozilla/DOMEventTargetHelper.h"
 #include "mozilla/dom/BluetoothGattBinding.h"
 #include "mozilla/dom/bluetooth/BluetoothCommon.h"
+#include "mozilla/dom/bluetooth/BluetoothGattService.h"
 #include "nsCOMPtr.h"
 
 namespace mozilla {
@@ -39,6 +40,11 @@ public:
     return mConnectionState;
   }
 
+  void GetServices(nsTArray<nsRefPtr<BluetoothGattService>>& aServices) const
+  {
+    aServices = mServices;
+  }
+
   /****************************************************************************
    * Event Handlers
    ***************************************************************************/
@@ -49,6 +55,8 @@ public:
    ***************************************************************************/
   already_AddRefed<Promise> Connect(ErrorResult& aRv);
   already_AddRefed<Promise> Disconnect(ErrorResult& aRv);
+  already_AddRefed<Promise> DiscoverServices(ErrorResult& aRv);
+  already_AddRefed<Promise> ReadRemoteRssi(ErrorResult& aRv);
 
   /****************************************************************************
    * Others
@@ -85,6 +93,15 @@ private:
    */
   void GenerateUuid(nsAString &aUuidString);
 
+  /**
+   * Add newly discovered GATT services into mServices and update the cache
+   * value of mServices.
+   *
+   * @param aValue [in] BluetoothValue which contains an array of
+   *                    BluetoothGattServiceId of all discovered services.
+   */
+  void HandleServicesDiscovered(const BluetoothValue& aValue);
+
   /****************************************************************************
    * Variables
    ***************************************************************************/
@@ -108,6 +125,16 @@ private:
    * Address of the remote device.
    */
   nsString mDeviceAddr;
+
+  /**
+   * Array of discovered services from the remote GATT server.
+   */
+  nsTArray<nsRefPtr<BluetoothGattService>> mServices;
+
+  /**
+   * Indicate whether there is ongoing discoverServices request or not.
+   */
+  bool mDiscoveringServices;
 };
 
 END_BLUETOOTH_NAMESPACE

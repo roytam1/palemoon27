558e8c786aa2a2aaaea2e224e4f13e7849576803
roytam1 <roytam@gmail.com>
2020-09-19 00:10:37 +0800

    partly import change from tenfourfox: - #622: M1660537 M1641487(modified) M1645492 (291688840)

--- dom/base/nsFrameLoader.cpp
+++ dom/base/nsFrameLoader.cpp
@@ -1987,6 +1987,10 @@ nsFrameLoader::CheckForRecursiveLoad(nsIURI* aURI)
 nsresult
 nsFrameLoader::GetWindowDimensions(nsIntRect& aRect)
 {
+  if (!mOwnerContent) {
+    return NS_ERROR_FAILURE;
+  }
+
   // Need to get outer window position here
   nsIDocument* doc = mOwnerContent->GetComposedDoc();
   if (!doc) {

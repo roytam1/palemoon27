043ed5895be38b88725f0f1c30f1aeaef3b15ca9
roytam1 <roytam@gmail.com>
2020-11-04 22:08:01 +0800

    workaround for crash when browsing www.heise.de by turning off unboxed plain objects

--- js/src/jit/JitOptions.cpp
+++ js/src/jit/JitOptions.cpp
@@ -170,7 +170,7 @@ JitOptions::JitOptions()
     SET_DEFAULT(smallFunctionMaxBytecodeLength_, 100);
 
     // Toggles whether unboxed plain objects can be created by the VM.
-    SET_DEFAULT(disableUnboxedObjects, false);
+    SET_DEFAULT(disableUnboxedObjects, true);
 }
 
 bool

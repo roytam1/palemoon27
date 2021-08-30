d43e6f58e1585d3cfab327b086282c86d1e25dfa
roytam1 <roytam@gmail.com>
2021-08-30 09:33:31 +0800

    import changes from `dev' branch of rmottola/Arctic-Fox: - pointer style (8365db72a) - remove stackDepth check not found anywhere in official gecko (98f65d05e) - Bug 1134198 - Call Debugger::onPop at the point that caused the frame to pop before any unwinding in the JIT. (r=jandem) (5aea98ae6) - Bug 1189750 - Remove unused JM-related PCCounts counters. r=bhackett (7410480af) - Bug 1198245 - IonMonkey: Lock helperthread before finishing ionbuilder, r=jandem (0f28ef66a) - Bug 1188620 - Use PersistentRooted for asyncActivation roots; r=fitzgen (4f6ae0e92) - Bug 1190446 - Update Coverage information in Baseline. r=jandem (95935926e) - Bug 1188129 - Use a universal constructor to create and init PersistentRooted; r=jonco (75db7ea8f) - Bug 1187767 - Ensure PLDHashTable's generation is always updated when the entry store is modified. r=froydnj. (e49936dad) - Bug 1189156 (part 1) - Don't use enumeration style for PLDHashTable::SizeOf{In,Ex}cludingThis(). r=froydnj. (3152ab914) - Bug 1189156 (part 2) - Don't use enumeration style for nsTHashtable::SizeOf{In,Ex}cludingThis(). r=erahm. (de21442b7) - Bug 1189156 (part 3) - Factor out FontTable better. r=jfkthame. (afdee9bd7) - Bug 1189156 (part 4) - Don't use enumeration style for nsBaseHashtable::SizeOf{In,Ex}cludingThis(). r=erahm,jfkthame. (32d72520f) - Bug 1189156 (part 5) - Add FontEntryTable typedef and factor out some related code. r=jfkthame. (9983134a9) - Bug 1137437 - move security/apps/ cert header generation to moz.build; r=mshal,keeler (2f7abb37d) - Bug 1180993 - Part 3: Correct use sites of functions which return already_AddRefed. r=ehsan (2a6289b6a)

--- js/src/vm/Interpreter.cpp
+++ js/src/vm/Interpreter.cpp
@@ -2011,7 +2011,7 @@ CASE(EnableInterruptsPseudoOpcode)
 
     if (script->hasScriptCounts()) {
         PCCounts counts = script->getPCCounts(REGS.pc);
-        counts.get(PCCounts::BASE_INTERP)++;
+        counts.numExec()++;
         moreInterrupts = true;
     }
 

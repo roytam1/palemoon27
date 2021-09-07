21fb6867b5fb71eae2375089bd5ea8b2f180fc0f
roytam1 <roytam@gmail.com>
2021-09-07 15:32:22 +0800

    import changes from `dev' branch of rmottola/Arctic-Fox: - Bug 1172889 - Remove unused nsFrameLoader::DestroyChild. r=smaug (36bbb4447) - Bug 1172889 - Remove unused nsFrameLoader::mContentParent. r=smaug (cee16d62f) - Bug 1172889 - Get current RenderFrameParent from TabParent directly. r=roc (b7090504e) - Bug 1176239, do tab navigation in the child when moving between the parent and child process, r=smaug (5c3d8213c) - Bug 1125325 followup: Add missing 'override' keyword to TabChild::GetInnerSize() and nsWindowRoot::AddBrowser(), RemoveBrowser(), EnumerateBrowsers(). rs=ehsan (3ea24684f) - reorder includes (d09953fd0) - Bug 1190245 - Separate mMessageManager creation and FrameScript loading. r=smaug (90eaa1612) - Bug 1177578 - Remove 'No docshells for remote frames' warning in nsFrameLoader::GetDocShell. r=smaug (de5770f1e) - Bug 1164616 - "nsFrameLoader::DelayedStartLoadingRunnable is unused".  r=josh (2472ccba6) - Bug 1172889 - Rename nsFrameLoader::GetCurrentRemoteFrame to nsFrameLoader::GetCurrentRenderFrame. r=roc (8563de171) - Bug 1172889 - Reorder nsFrameLoader member declarations. r=smaug (535d6426d) - Bug 1190245 - Make the MaybeCreateDocShell using code path easier to follow. r=smaug (0d96f4663) - Bug 1190245 - Make frame script loading and browser-api initialization easier to follow. r=smaug (7993cfe00)

--- dom/base/nsFrameLoader.h
+++ dom/base/nsFrameLoader.h
@@ -23,8 +23,8 @@
 #include "mozilla/dom/Element.h"
 #include "mozilla/Attributes.h"
 #include "nsStubMutationObserver.h"
-#include "nsIFrame.h"
 #include "Units.h"
+#include "nsIFrame.h"
 
 class nsIURI;
 class nsSubDocumentFrame;
@@ -61,11 +61,6 @@ class nsFrameLoader final : public nsIFrameLoader,
   typedef mozilla::dom::PBrowserParent PBrowserParent;
   typedef mozilla::dom::TabParent TabParent;
   typedef mozilla::layout::RenderFrameParent RenderFrameParent;
-
-protected:
-  nsFrameLoader(mozilla::dom::Element* aOwner, bool aNetworkCreated);
-
-  ~nsFrameLoader();
 
 public:
   static nsFrameLoader* Create(mozilla::dom::Element* aOwner,
@@ -131,9 +126,6 @@ public:
                                      nsRefPtr<nsFrameLoader>& aFirstToSwap,
                                      nsRefPtr<nsFrameLoader>& aSecondToSwap);
 
-  // When IPC is enabled, destroy any associated child process.
-  void DestroyChild();
-
   /**
    * Return the primary frame for our owning content, or null if it
    * can't be found.
@@ -150,7 +142,7 @@ public:
   nsIDocument* GetOwnerDoc() const
   { return mOwnerContent ? mOwnerContent->OwnerDoc() : nullptr; }
 
-  PBrowserParent* GetRemoteBrowser();
+  PBrowserParent* GetRemoteBrowser() const;
 
   /**
    * The "current" render frame is the one on which the most recent
@@ -166,20 +158,8 @@ public:
    * returned.  (In-process <browser> behaves similarly, and this
    * behavior seems desirable.)
    */
-  RenderFrameParent* GetCurrentRemoteFrame() const
-  {
-    return mCurrentRemoteFrame;
-  }
+  RenderFrameParent* GetCurrentRenderFrame() const;
 
-  /**
-   * |aFrame| can be null.  If non-null, it must be the remote frame
-   * on which the most recent layer transaction completed for this's
-   * <browser>.
-   */
-  void SetCurrentRemoteFrame(RenderFrameParent* aFrame)
-  {
-    mCurrentRemoteFrame = aFrame;
-  }
   nsFrameMessageManager* GetFrameMessageManager() { return mMessageManager; }
 
   mozilla::dom::Element* GetOwnerContent() { return mOwnerContent; }
@@ -190,10 +170,10 @@ public:
   /**
    * Tell this FrameLoader to use a particular remote browser.
    *
-   * This will assert if mRemoteBrowser or mCurrentRemoteFrame is non-null.  In
-   * practice, this means you can't have successfully run TryRemoteBrowser() on
-   * this object, which means you can't have called ShowRemoteFrame() or
-   * ReallyStartLoading().
+   * This will assert if mRemoteBrowser is non-null.  In practice,
+   * this means you can't have successfully run TryRemoteBrowser() on
+   * this object, which means you can't have called ShowRemoteFrame()
+   * or ReallyStartLoading().
    */
   void SetRemoteBrowser(nsITabParent* aTabParent);
 
@@ -231,11 +211,22 @@ public:
   // Properly retrieves documentSize of any subdocument type.
   nsresult GetWindowDimensions(nsIntRect& aRect);
 
+  // public because a callback needs these.
+  nsRefPtr<nsFrameMessageManager> mMessageManager;
+  nsCOMPtr<nsIInProcessContentFrameMessageManager> mChildMessageManager;
+
 private:
+  nsFrameLoader(mozilla::dom::Element* aOwner, bool aNetworkCreated);
+  ~nsFrameLoader();
 
   void SetOwnerContent(mozilla::dom::Element* aContent);
 
   bool ShouldUseRemoteProcess();
+
+  /**
+   * Return true if the frame is a remote frame. Return false otherwise
+   */
+  bool IsRemoteFrame();
 
   /**
    * Is this a frameloader for a bona fide <iframe mozbrowser> or
@@ -285,6 +276,7 @@ private:
    */
   nsresult MaybeCreateDocShell();
   nsresult EnsureMessageManager();
+  nsresult ReallyLoadFrameScripts();
 
   // Updates the subdocument position and size. This gets called only
   // when we have our own in-process DocShell.
@@ -328,10 +320,6 @@ private:
   // Note: this variable must be modified only by ResetPermissionManagerStatus()
   uint32_t mAppIdSentToPermissionManager;
 
-public:
-  // public because a callback needs these.
-  nsRefPtr<nsFrameMessageManager> mMessageManager;
-  nsCOMPtr<nsIInProcessContentFrameMessageManager> mChildMessageManager;
 private:
   // Stores the root frame of the subdocument while the subdocument is being
   // reframed. Used to restore the presentation after reframing.
@@ -342,6 +330,13 @@ private:
   // enables us to detect whether the frame has moved documents during
   // a reframe, so that we know not to restore the presentation.
   nsCOMPtr<nsIDocument> mContainerDocWhileDetached;
+
+  TabParent* mRemoteBrowser;
+  uint64_t mChildID;
+
+  // See nsIFrameLoader.idl. EVENT_MODE_NORMAL_DISPATCH automatically
+  // forwards some input events to out-of-process content.
+  uint32_t mEventMode;
 
   bool mIsPrerendered : 1;
   bool mDepthTooGreat : 1;
@@ -366,17 +361,6 @@ private:
   // whether this frameloader's <iframe mozbrowser> is setVisible(true)'ed, and
   // doesn't necessarily correlate with docshell/document visibility.
   bool mVisible : 1;
-
-  // The ContentParent associated with mRemoteBrowser.  This was added as a
-  // strong ref in bug 545237, and we're not sure if we can get rid of it.
-  nsRefPtr<mozilla::dom::nsIContentParent> mContentParent;
-  RenderFrameParent* mCurrentRemoteFrame;
-  TabParent* mRemoteBrowser;
-  uint64_t mChildID;
-
-  // See nsIFrameLoader.idl. EVENT_MODE_NORMAL_DISPATCH automatically
-  // forwards some input events to out-of-process content.
-  uint32_t mEventMode;
 };
 
 #endif

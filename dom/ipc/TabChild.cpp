80aa647a5d25bfc6b37c66a412cefbd5d71c19ec
roytam1 <roytam@gmail.com>
2022-03-10 14:34:01 +0800

    import changes from `dev' branch of rmottola/Arctic-Fox: - code style, spacing (d4f024213a) - Bug 1205586 - new Date().toLocale{,Date,Time}String() should return appropriately differing strings as the local time zone/default locale change. r=till (cf46706e94) - Bug 1204025 - Change Symbol.length to be 0 instead of 1. r=jorendorff (f1e3f45756) - remove some PM esr38 diffs (f38809c3fa) - Bug 1177018 - Send mouse move events generated via nsIPresShell::SynthesizeMouseMove() to the child process through a different IPDL message than real mouse move events. r=smaug (7029fe2d3b) - Bug 1186135 - change active flag per channel. r=ehsan (239d99b7cc) - Bug 1175940 - UIResolutionChanged should not trigger sync messages from content to chrome. r=mconley (147fbfd33a) - Bug 1156662 - Identical code for different branches. r=bz (8487a27847) - Bug 1183954 - Fix Notification.data structured cloning on workers. r=robertbindar,mccr8 (82187ef6c2) - Bug 1189389 - nsIStructuredCloneContainer should use StructuredCloneHelper, r=smaug (6cddb9453a)

--- dom/ipc/TabChild.cpp
+++ dom/ipc/TabChild.cpp
@@ -8,7 +8,6 @@
 
 #include "TabChild.h"
 
-#include "AudioChannelService.h"
 #include "gfxPrefs.h"
 #ifdef ACCESSIBILITY
 #include "mozilla/a11y/DocAccessibleChild.h"
@@ -601,7 +600,6 @@ TabChild::TabChild(nsIContentChild* aManager,
   , mDefaultScale(0)
   , mIPCOpen(true)
   , mParentIsActive(false)
-  , mAudioChannelActive(false)
 {
   // In the general case having the TabParent tell us if APZ is enabled or not
   // doesn't really work because the TabParent itself may not have a reference
@@ -630,6 +628,10 @@ TabChild::TabChild(nsIContentChild* aManager,
 
       observerService->AddObserver(this, topic.get(), false);
     }
+  }
+
+  for (uint32_t idx = 0; idx < NUMBER_OF_AUDIO_CHANNELS; idx++) {
+    mAudioChannelsActive.AppendElement(false);
   }
 }
 
@@ -714,8 +716,8 @@ TabChild::Observe(nsISupports *aSubject,
 
     nsAutoString activeStr(aData);
     bool active = activeStr.EqualsLiteral("active");
-    if (active != mAudioChannelActive) {
-      mAudioChannelActive = active;
+    if (active != mAudioChannelsActive[audioChannel]) {
+      mAudioChannelsActive[audioChannel] = active;
       unused << SendAudioChannelActivityNotification(audioChannel, active);
     }
   }
@@ -755,7 +757,7 @@ TabChild::Init()
 
   nsCOMPtr<nsIDocShellTreeItem> docShellItem(do_QueryInterface(WebNavigation()));
   docShellItem->SetItemType(nsIDocShellTreeItem::typeContentWrapper);
-  
+
   nsCOMPtr<nsIBaseWindow> baseWindow = do_QueryInterface(WebNavigation());
   if (!baseWindow) {
     NS_ERROR("mWebNav doesn't QI to nsIBaseWindow");
@@ -1876,6 +1878,12 @@ TabChild::RecvRealMouseMoveEvent(const WidgetMouseEvent& event)
 }
 
 bool
+TabChild::RecvSynthMouseMoveEvent(const WidgetMouseEvent& event)
+{
+  return RecvRealMouseButtonEvent(event);
+}
+
+bool
 TabChild::RecvRealMouseButtonEvent(const WidgetMouseEvent& event)
 {
   WidgetMouseEvent localEvent(event);
@@ -2939,12 +2947,12 @@ TabChild::RecvRequestNotifyAfterRemotePaint()
 }
 
 bool
-TabChild::RecvUIResolutionChanged()
+TabChild::RecvUIResolutionChanged(const float& aDpi, const double& aScale)
 {
   ScreenIntSize oldScreenSize = GetInnerSize();
   mDPI = 0;
   mDefaultScale = 0;
-  static_cast<PuppetWidget*>(mPuppetWidget.get())->ClearBackingScaleCache();
+  static_cast<PuppetWidget*>(mPuppetWidget.get())->UpdateBackingScaleCache(aDpi, aScale);
   nsCOMPtr<nsIDocument> document(GetDocument());
   nsCOMPtr<nsIPresShell> presShell = document->GetShell();
   if (presShell) {

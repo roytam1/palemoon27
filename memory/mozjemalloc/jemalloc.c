522696571d0fae7ee80fb8e43901952939f64a8e
roytam1 <roytam@gmail.com>
2023-04-14 16:15:57 +0800

    import from UXP: Issue #2184 - Increase mozjemalloc page cache size from 1 MiB to 16 MiB. (d8cd769c)

--- memory/mozjemalloc/jemalloc.c
+++ memory/mozjemalloc/jemalloc.c
@@ -542,7 +542,7 @@ static const bool isthreaded = true;
  */
 #define	CHUNK_2POW_DEFAULT	20
 /* Maximum number of dirty pages per arena. */
-#define	DIRTY_MAX_DEFAULT	(1U << 8)
+#define	DIRTY_MAX_DEFAULT	(1U << 12)
 
 /*
  * Maximum size of L1 cache line.  This is used to avoid cache line aliasing,

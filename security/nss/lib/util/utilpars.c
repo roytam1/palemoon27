b7271dae6af93700c5c52e121a152c5148b2bcf2
roytam1 <roytam@gmail.com>
2024-05-25 07:51:08 +0800

    nss: use dbm by default

--- security/nss/lib/util/utilpars.c
+++ security/nss/lib/util/utilpars.c
@@ -1198,7 +1198,11 @@ _NSSUTIL_EvaluateConfigDir(const char *configdir,
     PRBool checkEnvDefaultDB = PR_FALSE;
     *appName = NULL;
     /* force the default */
+#ifdef NSS_DISABLE_DBM
     dbType = NSS_DB_TYPE_SQL;
+#else
+    dbType = NSS_DB_TYPE_LEGACY;
+#endif
     if (configdir == NULL) {
         checkEnvDefaultDB = PR_TRUE;
     } else if (PORT_Strncmp(configdir, MULTIACCESS, sizeof(MULTIACCESS) - 1) == 0) {

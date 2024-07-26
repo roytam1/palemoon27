cbc917768b205498844c71f4ff1228dd54d5e37f
roytam1 <roytam@gmail.com>
2024-07-26 21:49:43 +0800

    import changes from `dev' branch of rmottola/Arctic-Fox: - Bug 1266578 - OOM crash if malloc fails in ProcessIncomingMessages(). r=billm (5eca51be20) - Bug 1261340: Don't refer to |ScopedFreePtrTraits| in |RawDBusConnection|, r=shuang (61996db57c) - Bug 1246931: Move |DBusWatcher| into its own files, r=shuang (a7fc4a68c3) - Bug 1246931: Add support for |RefPtr<DBusConnection>| and convert callers, r=shuang (6c07d3e8d3) - Bug 1246931: Add support for |RefPtr<DBusMessage>|, r=shuang (c0ed14cc2b) - Bug 1246931: Add support for |RefPtr<DBusPendingCall>|, r=shuang (a007a15699) - Bug 1246931: Use |DBusConnection| in |DBusWatcher|, r=shuang (11b5eaf535) - Bug 1246931: Add DBus I/O helpers, r=shuang (be249fa65b) - Bug 1264398 - Avoid extra assign() on windows in IPC code (r=jld) (0f31b798a3) - Bug 1261231: Fix shutdown leak in imgLoader::GetInstance. r=gabor (96e5143c41) - Bug 1256999 - Use nsIDocument for ImageCacheKey. r=bz r=seth (cd4765ce34) - Bug 1257101. imgFrame::IsImageComplete says whether we've had pixels decoded to the whole image rect, but it's used to check if the frame is finished decoding. These are different things when the image has more than one progress pass. r=seth (06d619410f) - Bug 1222596. If RasterImage::LookupFrame does (some) sync decoding and encouters an error we don't want to return the surface with an error. r=seth (c4dbc8e0d5) - Bug 763784 - Make VectorImage::GetAnimated check for CSS animations. r=dholbert (4d0e5b88eb) - Bug 1210745 - Update CheckProgressConsistency() to match current ImageLib behavior. r=tn (2317b24fcc) - Bug 1249576. Add crashtest. (485f03120b) - Bug 1251091. Add crashtest. (7e1682a1e6) - Bug 1253362. SVGDocumentWrapper::IsAnimated can be called after SVGDocumentWrapper::DestroyViewer so null check mViewer. r=dholbert (2b4a1c4619) - Bug 1210745. Change image progress asserts to allow transparency to be posted after the size is posted. r=seth (699f3c5496) - Bug 1209780. Use the DrawResult return value of imgIContainer::Draw in the cocoa code. r=seth (abaea789e3) - No Bug - Remove some unnecessary SVGImageContext.h includes and add comments. r=sparky (8232661a23) - cleanup style (de33e4bfa8) - Bug 860857, support custom datatransfer types using a special type, r=smaug,jmathies,mstange (cf7e19deb6) - Bug 1248459 - Don't query out-of-bounds selection; r=masayuki (86c127f143) - Bug 1261671 - ContentEventHandler::ConvertToRootRelativeOffset() should return the root-relative result in the frame's own appUnits, not the root's appUnits in the case when they're different. r=masayuki (2cd72b5ebc) - Bug 1266702 - Clean up formatting in dom/events/DataTransfer.* and mark some methods const, r=jwatt (bd439cdad5)

--- image/RasterImage.cpp
+++ image/RasterImage.cpp
@@ -375,7 +375,16 @@ RasterImage::LookupFrame(uint32_t aFrameNum,
   // async decoder that's currently running, the contents of the frame may not
   // be available yet. Make sure we get everything.
   if (mHasSourceData && (aFlags & FLAG_SYNC_DECODE)) {
-    result.DrawableRef()->WaitUntilComplete();
+    result.DrawableRef()->WaitUntilFinished();
+  }
+
+  // If we could have done some decoding in this function we need to check if
+  // that decoding encountered an error and hence aborted the surface. We want
+  // to avoid calling IsAborted if we weren't passed any sync decode flag because
+  // IsAborted acquires the monitor for the imgFrame.
+  if (aFlags & (FLAG_SYNC_DECODE | FLAG_SYNC_DECODE_IF_FAST) &&
+    result.DrawableRef()->IsAborted()) {
+    return DrawableFrameRef();
   }
 
   return Move(result.DrawableRef());
@@ -622,7 +631,7 @@ RasterImage::GetFrameInternal(const IntSize& aSize,
     frameSurf = CopyFrame(aWhichFrame, aFlags);
   }
 
-  if (!frameRef->IsImageComplete()) {
+  if (!frameRef->IsFinished()) {
     return MakePair(DrawResult::INCOMPLETE, Move(frameSurf));
   }
 
@@ -1454,7 +1463,7 @@ RasterImage::DrawInternal(DrawableFrameRef&& aFrameRef,
 {
   gfxContextMatrixAutoSaveRestore saveMatrix(aContext);
   ImageRegion region(aRegion);
-  bool frameIsComplete = aFrameRef->IsImageComplete();
+  bool frameIsFinished = aFrameRef->IsFinished();
 
   // By now we may have a frame with the requested size. If not, we need to
   // adjust the drawing parameters accordingly.
@@ -1473,7 +1482,7 @@ RasterImage::DrawInternal(DrawableFrameRef&& aFrameRef,
     RecoverFromInvalidFrames(aSize, aFlags);
     return DrawResult::TEMPORARY_ERROR;
   }
-  if (!frameIsComplete) {
+  if (!frameIsFinished) {
     return DrawResult::INCOMPLETE;
   }
   if (couldRedecodeForBetterFrame) {
